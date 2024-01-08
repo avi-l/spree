15cf377150a7f6c4dc49327425c6efd340023325
Avi <avi.leeker@gmail.com>
2024-01-08 18:56:35 +0200

    build debug

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -8,7 +8,6 @@ const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  "Access-Control-Allow-Credentials": "true",
 };
 
 export async function OPTIONS() {
